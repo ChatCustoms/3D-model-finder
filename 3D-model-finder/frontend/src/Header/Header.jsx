4a9c69297a895e777923255051044d52f2140bf3
ChatCustoms <stephano.chatham@gmail.com>
2025-08-13 14:07:28 -0400

    commit for the frontend final project with some structure.

---
+++ 3D-model-finder/frontend/src/Header/Header.jsx
@@ -0,0 +1,69 @@
+import "./Header.css";
+import logo from "../assets/ChatCustom_Logo_Dark.svg";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch.jsx";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext.jsx";
+
+function Header({ handleAddClick, weatherData, isOpen, onLogin, onRegister }) {
+  const currentUser = useContext(CurrentUserContext);
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  console.log("Header - currentUser:", currentUser);
+  return (
+    <header className="header">
+      <div className="header__container">
+        <Link to="/">
+          {" "}
+          <img className="header__logo" src={logo} alt="WTWR" />
+        </Link>
+        <p className="header__date-and-location">
+          {currentDate}, {weatherData.city}
+        </p>
+      </div>
+      <ToggleSwitch />
+      { currentUser && (
+      <button
+        type="button"
+        onClick={handleAddClick}
+        className="header__add-clothes-btn"
+      >
+        + Add clothes
+      </button>
+      )}
+      {!currentUser ? (
+        <div className="header__auth-buttons">
+          <button type="button" className="header__login-btn" onClick={onLogin}>
+            Log In
+          </button>
+          <button
+            type="button"
+            className="header__register-btn"
+            onClick={onRegister}
+          >
+            Sign Up
+          </button>
+        </div>
+      ) : (
+        <Link to="/profile" className="header__user-container">
+          <p className="header__username">{currentUser.name}</p>
+          {currentUser.avatar ? (
+            <img
+              src={currentUser.avatar}
+              alt={currentUser.name}
+              className="header__avatar"
+            />
+          ) : (
+            <div className="header__avatar-placeholder">
+              {currentUser.name.charAt(0)}
+            </div>
+          )}
+        </Link>
+      )}
+    </header>
+  );
+}
+
+export default Header;
