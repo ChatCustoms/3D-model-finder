5c517f2c5e9eef3ea86400cd4865cd88c2b7550d
ChatCustoms <stephano.chatham@gmail.com>
2025-08-28 18:23:45 -0400

    Fixed the eslint errors .

--- frontend/src/Components/ProtectedRoute.jsx
+++ frontend/src/Components/ProtectedRoute.jsx
@@ -3,12 +3,15 @@ import PropTypes from "prop-types";
 
 const ProtectedRoute = ({ children, loggedIn }) => {
   console.log("ProtectedRoute - loggedIn:", loggedIn);
+  if (loggedIn === undefined || loggedIn === null) {
+    return null;
+  }
   return loggedIn ? children : <Navigate to="/" replace />;
 };
 
 ProtectedRoute.propTypes = {
   children: PropTypes.node.isRequired,
-  loggedIn: PropTypes.bool.isRequired,
+  loggedIn: PropTypes.bool,
 };
 
 export default ProtectedRoute;
