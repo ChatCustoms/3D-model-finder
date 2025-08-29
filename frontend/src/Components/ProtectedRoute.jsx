f71149d250a5e4289a7e98b53be91c750f99cb00
ChatCustoms <stephano.chatham@gmail.com>
2025-08-28 21:44:47 -0400

    Adding a content box behind elements so you can see it better, fixed the profile page not loading.

--- frontend/src/Components/ProtectedRoute.jsx
+++ frontend/src/Components/ProtectedRoute.jsx
@@ -1,32 +1,18 @@
 import { Navigate, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 
-// const ProtectedRoute = ({ children, loggedIn }) => {
-//   const location = useLocation();
-//   console.log("ProtectedRoute - loggedIn:", loggedIn);
+const ProtectedRoute = ({ children, loggedIn }) => {
+  const location = useLocation();
+  console.log("ProtectedRoute - loggedIn:", loggedIn);
 
-//   if (loggedIn === undefined || loggedIn === null) {
-//     return null;
-//   }
-
-//   if (!loggedIn) {
-//     return <Navigate to="/" replace state={{ from: location }} />;
-//   }
-
-//   return children;
-// };
-
-export default function ProtectedRoute(props) {
-  const { loggedIn, children } = props;
-  console.log("ProtectedRoute props:", props); // <-- should show { loggedIn: true/false, children: ... }
-
-  // handle loading/null
-  if (loggedIn === undefined || loggedIn === null) return null;
+  if (loggedIn === undefined || loggedIn === null) {
+    return null;
+  }
 
   if (!loggedIn) {
-    const loc = useLocation();
-    return <Navigate to="/" replace state={{ from: loc }} />;
+    return <Navigate to="/" replace state={{ from: location }} />;
   }
+
   return children;
 };
 
@@ -35,4 +21,4 @@ ProtectedRoute.propTypes = {
   loggedIn: PropTypes.bool,
 };
 
-// export default ProtectedRoute;
+export default ProtectedRoute;
